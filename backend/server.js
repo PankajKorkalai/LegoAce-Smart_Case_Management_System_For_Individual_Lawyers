afbee99f1c14d0b75b8ada3f633147cf7893f788
Harshchafle <chafleharsh@gmail.com>
2026-04-06 16:50:12 +0530

    homee done

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,9 @@ const connectDB = require("./db/db");
 const uploadRouter = require("./routes/upload");
 const documentsRouter = require("./routes/documents");
 const userRouter = require("./routes/user");
-<<<<<<< HEAD
 const clientRouter = require("./routes/client");
 const profileRouter = require("./routes/profile");
-=======
-// 1. ADD THIS IMPORT
 const scheduleRouter = require("./routes/schedule"); 
->>>>>>> 4e73efc0a37f8ff851245d88d97dcfc3033ae2d9
 
 connectDB();
 
@@ -34,8 +30,6 @@ app.use("/api", documentsRouter);
 app.use("/user", userRouter);
 app.use("/api", clientRouter);
 app.use("/api", profileRouter);
-
-// 2. MOUNT THE NEW ROUTER
 app.use("/api/meetings", scheduleRouter); 
 
 const PORT = process.env.PORT || 5000;
